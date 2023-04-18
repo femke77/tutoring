b83fa7e204c9e0b5063f2b02ace77accd5e85718
femke77 <megan.meyers.388@gmail.com>
2023-04-17 20:00:07 -0700

    basic server with user model, jwt, connection running and introspecting

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,48 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Blog } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+	Query: {
+	
+		me: async (_, __, context) => {
+			if (context.user) {
+				const userData = await User.findOne({ _id: context.user._id }).select('-__v -password').populate('blogs');
+
+				return userData;
+			}
+
+			throw new AuthenticationError('Not logged in');
+		}
+	},
+
+	Mutation: {
+		addUser: async (_, args) => {
+		  const user = await User.create(args);
+		  const token = signToken(user);
+		  return { token, user };
+		},
+
+		login: async (_, { email, password }) => {
+		  const user = await User.findOne({ email });
+	
+		  if (!user) {
+			throw new AuthenticationError('No user found with this email address');
+		  }
+	
+		  const correctPw = await user.isCorrectPassword(password);
+	
+		  if (!correctPw) {
+			throw new AuthenticationError('Incorrect credentials');
+		  }
+	
+		  const token = signToken(user);
+	
+		  return { token, user };
+		},
+
+		
+	  },
+};
+
+module.exports = resolvers;
