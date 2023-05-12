296a0d0ab42cb665080ce9795433296ac26a0233
femke77 <megan.meyers.388@gmail.com>
2023-05-11 22:39:39 -0700

    secondDrawer minor changes

--- client/src/components/SecondDrawer.js
+++ client/src/components/SecondDrawer.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Box, Button } from "@mui/material";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Collapse from "@mui/material/Collapse";
 
 export default function SecondDrawer() {
   const [open, setOpen] = useState(true);
@@ -11,18 +12,23 @@ export default function SecondDrawer() {
 
   return (
     <div className="d-flex">
+      {/* don't use conditional rendering, use the Collapse transition */}
       {open && (
-        <Box
-          sx={{
-            background: "black",
-            color: "white",
-            width: "548px",
-            height: "100vh",
-            p: 3,
-          }}
-        >
-          <div className="mt-5">Seccond drawer here, line me up with nav! </div>
-        </Box>
+        <Collapse in={open}>
+          <Box
+            sx={{
+              background: "black",
+              color: "white",
+              width: "548px",
+              height: "100vh",
+              p: 3,
+            }}
+          >
+            <div className="mt-5">
+              Second drawer here, line me up with nav!{" "}
+            </div>
+          </Box>
+        </Collapse>
       )}
 
       <Box
