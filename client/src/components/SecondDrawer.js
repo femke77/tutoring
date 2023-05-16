7fb199fa67ba401a82cf001f166e9342590ff3bf
femke77 <megan.meyers.388@gmail.com>
2023-05-15 18:57:44 -0700

    routing now in second drawer, example background image in place for seeing what the carousel will look like

--- client/src/components/SecondDrawer.js
+++ client/src/components/SecondDrawer.js
@@ -3,6 +3,14 @@ import { Box, Button, hexToRgb } from "@mui/material";
 import Collapse from "@mui/material/Collapse";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import CloseIcon from "@mui/icons-material/Close";
+import Calendly from "./Calendly";
+import About from "./About";
+import ContactBS from "./Contact";
+import Terms from "./Terms";
+import NoMatch from "./NoMatch";
+import Blog from "./Blog";
+import Privacy from "./PrivacyPolicy";
+import { BrowserRouter as _, Routes, Route } from "react-router-dom";
 
 export default function SecondDrawer() {
   const [open, setOpen] = useState(true);
@@ -13,27 +21,32 @@ export default function SecondDrawer() {
 
   return (
     <div className="d-flex">
- 
-        <Collapse in={open} orientation="horizontal" timeout={500}>
-          <Box
-            sx={{
-              background: "black",
-              opacity: "0.8",
-              color: "white",
-            //   width is perfect for 768, adjust for smaller
-              width: "548px",  
-              height: "100vh",
-              p: 3,
-            }}
-          >
-            <div className="content">
-              Content rendered through this drawer. Screenshots of projects in
-              the background. On smaller screens this will be the full page view.
-    
-            </div>
-          </Box>
-        </Collapse>
-  
+      <Collapse in={open} orientation="horizontal" timeout={500}>
+        <Box
+          sx={{
+            background: "black",
+            opacity: "0.8",
+            color: "white",
+            width: "650px",
+            height: "100%",
+            minHeight: "100vh",
+            p: 3,
+          }}
+        >
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Calendly />} />
+              <Route path="/contact" element={<ContactBS />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+        </Box>
+      </Collapse>
+
       <Box
         sx={{
           background: "black",
