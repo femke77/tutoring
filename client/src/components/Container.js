ccaf14f49567ab11e8dc886a4533daa26b148fca
femke77 <megan.meyers.388@gmail.com>
2024-05-30 23:41:25 -0700

    preloaded images and really for real fixed flicker caused by repeated network requests

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useLayoutEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Calendly from "./Calendly";
 import About from "./About";
@@ -8,20 +8,20 @@ import NoMatch from "./NoMatch";
 import Blog from "./Blog";
 import Privacy from "./PrivacyPolicy";
 import SecondDrawer from "./SecondDrawer";
-import MX from "../assets/musicxpansion.png";
-import Cat from "../assets/word-guess.png";
-import Crave from '../assets/crave.png'
-import Portfolio from '../assets/portfolio.png'
-import Lego from '../assets/lego.png'
-import Car from '../assets/car.png'
+// import MX from "../assets/musicxpansion.png";
+// import Cat from "../assets/word-guess.png";
+// import Crave from '../assets/crave.png'
+// import Portfolio from '../assets/portfolio.png'
+// import Lego from '../assets/lego.png'
+// import Car from '../assets/car.png'
 
 function Container() {
-  const images = [MX, Cat, Crave, Portfolio, Lego, Car]
+  const images = ["musicxpansion.png", "word-guess.png", "lego.png", "crave.png", "portfolio.png", "car.png"]
   const [idx, setIdx] = useState(0)
   const intervalRef = useRef(null)
 
   const bg = {
-    backgroundImage: `url(${images[idx]})`,
+    backgroundImage: `url(/${images[idx]})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center", 
@@ -29,10 +29,11 @@ function Container() {
   };
 
   const update = () => {
-    setIdx(prev => ((prev === 5) ? 0 : prev + 1))
+    setIdx(prev => ((prev === images.length -1) ? 0 : prev + 1))
   };
 
-  useLayoutEffect(() => {
+  useEffect(() => {
+
     console.log(idx);
     intervalRef.current = setInterval(() => {
       update()
